1b8d97b7d654248b075129925ba00040bf70465c
alejo.basilio <alejo.basilio@viewnext.com>
2025-03-17 09:52:17 +0100

    first commit

---
+++ src/main/java/com/alejobasilio/receta_xml/RecetaXmlApplication.java
@@ -0,0 +1,13 @@
+package com.alejobasilio.receta_xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetaXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetaXmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejobasilio/receta_xml/RecetaXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejobasilio.receta_xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetaXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
