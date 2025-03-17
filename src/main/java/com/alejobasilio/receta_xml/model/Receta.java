2c7e1e10a60e50cc5be26eb7195611da5db2375f
alejo.basilio <alejo.basilio@viewnext.com>
2025-03-17 17:08:59 +0100

    Añadidas etiquetas xml

--- src/main/java/com/alejobasilio/receta_xml/model/Receta.java
+++ src/main/java/com/alejobasilio/receta_xml/model/Receta.java
@@ -1,5 +1,8 @@
 package com.alejobasilio.receta_xml.model;
 
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,13 +12,20 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlRootElement(name = "receta")
 public class Receta {
 
+	@XmlAttribute(name = "id")
 	private Long id;
+	@XmlElement(name = "nombre")
 	private String nombre;
+	@XmlElement(name = "ingrediente")
 	private String ingrediente;
+	@XmlElement(name = "cantidad")
 	private Integer cantidad;
+	@XmlElement(name = "ingredienteOpc")
 	private String ingredienteOpc;
+	@XmlElement(name = "cantidadOpc")
 	private Integer cantidadOpc;
 	
 }
